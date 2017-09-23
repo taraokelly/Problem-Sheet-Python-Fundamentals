29de4d109da177d2872565a623fb23d5f827b450
taraokelly <taraokelly.dev@gmail.com>
2017-09-23 16:42:17 +0100

    Solution No. 2: strftime

--- solutions/current_time.py
+++ solutions/current_time.py
@@ -2,4 +2,6 @@ import datetime
 
 now = datetime.datetime.now()
 
-print("Current date and time: " + str(now))
+#print("Current date and time: " + str(now))
+
+print("Current date and time: " + now.strftime("%Y-%m-%d %H:%M"))
