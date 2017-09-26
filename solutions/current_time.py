79b39892f88a651f32325bc8abe90c22a2ea61de
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 13:24:56 +0100

    Solution No. 2: clean up

--- solutions/current_time.py
+++ solutions/current_time.py
@@ -1,7 +1,14 @@
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 1. Write a program that prints the current time and date to the console.
+
+# https://docs.python.org/3/library/datetime.html
 import datetime
 
+# Returns the current local date and time
 now = datetime.datetime.now()
 
-#print("Current date and time: " + str(now))
-
+# strftime formats string based on args
 print("Current date and time: " + now.strftime("%Y-%m-%d %H:%M"))
