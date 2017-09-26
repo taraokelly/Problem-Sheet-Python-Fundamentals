0f351be4a549eb56200b84580a5bc749ce0e7891
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 14:37:27 +0100

    Solution No. 8

---
+++ solutions/merge_sort_list.py
@@ -0,0 +1,11 @@
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 8. Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5] → [1,2,3,4,5,6].
+
+l1 = [1,4,6]
+l2 = [2,3,5]
+
+#https://docs.python.org/3/howto/sorting.html
+print(sorted(l1 + l2))
