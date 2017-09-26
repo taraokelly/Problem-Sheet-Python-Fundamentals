7b4a2ec6a7c0144597d79c23f46a5cd4bcaca68d
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 13:28:01 +0100

    Solution No. 1: clean up

--- solutions/hello_world.py
+++ solutions/hello_world.py
@@ -1,1 +1,6 @@
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 1. Write a program that prints "Hello, world!" to the screen.
 print("Hello, world!")
