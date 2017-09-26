103ad3a2ae7a6229945e4f1b2a67776d9a4a5160
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 13:30:58 +0100

    Solution No. 3: clean up

--- solutions/fizzbuzz.py
+++ solutions/fizzbuzz.py
@@ -1,4 +1,11 @@
-for x in range(1,101):
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 3. Write a program that prints the numbers from 1 to 100, except for the following conditions. For multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+# http://pythoncentral.io/pythons-range-function-explained/
+for x in range(1,100+1):
     if (x % 3 == 0) & (x % 5 == 0):
         print("FizzBuzz")
     elif x % 3 == 0:
