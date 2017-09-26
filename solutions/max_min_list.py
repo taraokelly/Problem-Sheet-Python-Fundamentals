44cecee79f3741fb9d7f73fbb83ae137a64499be
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 13:58:17 +0100

    Solution No. 6: clean up

--- solutions/max_min_list.py
+++ solutions/max_min_list.py
@@ -1,7 +1,18 @@
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 6. Write a function that returns the largest and smallest elements in a list.
+
 user_list = []
-# get user input and analyse use min and max functions
+
+# get user input
 n = int(input('How many numbers: '))
 for x in range(n):
     numbers = int(input('Enter number: \n'))
     user_list.append(numbers)
+
+# use min and max functions
+# https://docs.python.org/3/library/functions.html#max
+# https://docs.python.org/3/library/functions.html#min
 print("Largest element in the list is :", max(user_list), "\nSmallest element in the list is :", min(user_list))
