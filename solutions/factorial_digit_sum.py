3ffb7310f112761aec69f6b1c0d81a4a9205e87c
taraokelly <taraokelly.dev@gmail.com>
2017-09-26 13:55:03 +0100

    Solution No. 4 & 5: clean up

--- solutions/factorial_digit_sum.py
+++ solutions/factorial_digit_sum.py
@@ -1,10 +1,17 @@
-n = 100;
-sum = 0;
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
 
+# Problem set: Python fundamentals
+# 4. Write a function that calculates the sum of the digits of the factorial of a number. n! means n x (n − 1) ... x 3 x 2 x 1. For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27. Find the sum of the digits in the number 100!
+
+n, sum = 100, 0;
+
+# http://pythoncentral.io/pythons-range-function-explained/
 for x in range(n,0,-1):
     if x == n:
         sum = n;
     else:
         sum = sum * x;
 
+# Print factorial
 print(sum)
--- solutions/guessing_game.py
+++ solutions/guessing_game.py
@@ -1,13 +1,17 @@
+# Tara O'Kelly - G00322214
+# Emerging Technologies, Year 4, Software Development, GMIT.
+
+# Problem set: Python fundamentals
+# 5. Write a guessing game where the user must guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
+
+# https://docs.python.org/3/library/random.html
 from random import randint
 
+# Returns a random integer n such that 1 <= n <= 10
 n = randint(1, 10)
-x = 0
-incorrect_inputs = []
-correct = 0;
-guess_cnt = 0;
+x, correct, guess_cnt, incorrect_inputs = 0, 0, 0, []
 
-# Get user input, analyse input and print result
-
+# Get user input, analyse input until correct answer is quessed
 while correct == 0:
     x = input("Enter a number: ")
     
@@ -24,6 +28,8 @@ while correct == 0:
             incorrect_inputs.append(int(x));
             guess_cnt+=1;
 
+# Print results
 print("Congrats! The answer was "+ str(n) + ".")
-print("Incorrect Guesses: " + str(guess_cnt))
-print(incorrect_inputs)
+if incorrect_inputs:
+    print("Incorrect Guesses: " + str(guess_cnt))
+    print(incorrect_inputs)
